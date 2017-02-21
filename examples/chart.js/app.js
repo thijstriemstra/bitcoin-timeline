29df9986f7e0736b90d904273c40f7c0b234d975
Thijs Triemstra <info@collab.nl>
2017-02-21 05:05:16 +0100

    add stats with backbone transaction collection

--- examples/chart.js/app.js
+++ examples/chart.js/app.js
@@ -44,43 +44,77 @@ function handleFileSelect()
 }
 
 
-var example = {
-        labels : [],
-        label : "Bitstamp BPI in USD",
-        data: []
+var myChart;
+var transactions = new Transactions();
+
+var bpi_chart_data = {
+    labels : [],
+    label : "Bitstamp BPI in USD",
+    data: []
 };
 
-function parseCSVDone(data) {
-    console.log("data: ", data);
 
-    
-}
+var transactions_chart_data = {
+    labels : [],
+    label : "Transaction in USD",
+    data: [],
+    rawData: []
+};
 
-function parseBPI(data) {
+
+function buildChart(data) {
     
-    //parseCSV(fr.result);
-        
     var ctx = document.getElementById("myChart");
-    
-    //console.log(data);
-    
-    for (var i in data.bpi) {
-        // console.log(i, data.bpi[i])
-        example.labels.push(i);
-        example.data.push(data.bpi[i]);
-    }
-
     // build graph
-    var myChart = new Chart(ctx, {
+    myChart = new Chart(ctx, {
         type: 'line',
         data: {
-            labels: example.labels,
+            labels: bpi_chart_data.labels,
             datasets: [{
-                label: example.label,
-                data: example.data
-            }]
-        },
-        options: {
+               label: bpi_chart_data.label,
+               data: bpi_chart_data.data,
+               strokeColor: "rgba(220,220,220,1)",
+               pointColor: "rgba(220,220,220,1)",
+               pointStrokeColor: "#fff",
+               lineTension: 0.1,
+               backgroundColor: "rgba(75,192,192,0.4)",
+               borderColor: "rgba(75,192,192,1)",
+               borderCapStyle: 'butt',
+               borderDash: [],
+               borderDashOffset: 0.0,
+               borderJoinStyle: 'miter',
+               pointBorderColor: "rgba(75,192,192,1)",
+               pointBorderWidth: 1,
+               pointHoverRadius: 5,
+               pointHoverBackgroundColor: "rgba(75,192,192,1)",
+               pointHoverBorderColor: "rgba(220,220,220,1)",
+               pointHoverBorderWidth: 2,
+               pointRadius: 1,
+            },
+            {
+               label: transactions_chart_data.label,
+               data: transactions_chart_data.data,
+               fillColor: "rgba(220,220,220,.2)",
+               pointColor: "rgba(151,187,205,1)",
+               pointStrokeColor: "#fff",
+               lineTension: 0.1,
+               backgroundColor: "rgba(151,187,205,1)",
+               borderColor: "rgba(151,187,205,1)",
+               borderCapStyle: 'butt',
+               borderDash: [],
+               borderDashOffset: 0.0,
+               borderJoinStyle: 'miter',
+               pointBorderColor: "rgba(151,187,205,1)",
+               pointBorderWidth: 1,
+               pointHoverRadius: 5,
+               pointHoverBackgroundColor: "rgba(151,187,205,1)",
+               pointHoverBorderColor: "rgba(220,220,220,1)",
+               pointHoverBorderWidth: 2,
+               pointRadius: 1,
+               spanGaps: true
+           }]
+         },
+         options: {
             scales: {
                 yAxes: [{
                     ticks: {
@@ -90,13 +124,20 @@ function parseBPI(data) {
             }
         }
     });
-    
 }
 
 
-// 1: load csv
-// 2: load bpi
-// 3: chart
+function parseBPI(data) {
+    //console.log(data);
+
+    for (var i in data.bpi) {
+        // label, e.g. 2014-07-04
+        bpi_chart_data.labels.push(i);
+        // data (price in dollars)
+        bpi_chart_data.data.push(parseFloat(data.bpi[i]));
+    }
+}
+
 
 function parseCSV(input) {
     //
@@ -105,43 +146,78 @@ function parseCSV(input) {
         header: true,
         step: function(results) {            
             var item = results.data[0];
-            
+
             // console.info(item);
-            
+
+            function pad(n){return n<10 ? '0'+n : n}
+
             var timestamp = Date.parse(item.Datetime.replace(".", ""));
             var now = new Date(timestamp);
             var year = now.getFullYear().toString();
             var month = (now.getMonth() + 1).toString();
             var day = now.getDate().toString();
-            var datestring = year + "-" + month + "-" + day;
-            
-            var value = item.Value.replace(" USD", "");
 
-            if (value) {
-                example.data.push(value);
-                example.labels.push(datestring);
-            } 
+            var transaction = new Transaction({
+                subType: item['Sub Type'],
+                value: parseFloat(item.Value.replace(" USD", "")),
+                amount: parseFloat(item.Amount.replace(" BTC", "")),
+                // Deposit/Market/Withdrawal
+                type: item.Type,
+                accountName: item.Account,
+                rate: parseFloat(item.Rate.replace(" USD", "")),
+                fee: parseFloat(item.Fee.replace(" USD", "")),
+                timestamp: timestamp,
+                datestring: year + "-" + pad(month) + "-" + pad(day)
+            });
+
+            if (transaction.get('value') && transaction.isBuy()) {
+                // label, e.g. 2014-07-04
+                transactions_chart_data.labels.push(transaction.get('datestring'));
+                transactions_chart_data.data.push(transaction.get('value'));
+            }
+
+            transactions_chart_data.rawData.push(transaction);
+            
+            transactions.add(transaction);
         },
-        
+
         complete: function(results) {
-            console.log('completed loading CSV.');
-            parseCSVDone(example);
+            console.log('Completed loading transactions CSV.');
+
+            // Market (Buy)
+            var total_spent = transactions.total_spent();
+            var total_bought = transactions.total_bought();
+            var total_fees = transactions.total_fees();
+
+            var lowest_rate = transactions.lowest_rate();
+            var highest_rate = transactions.highest_rate();
+
+            console.log('total spent (USD)', total_spent);
+            console.log('total bought (BTC)', total_bought);
+            console.log('total fees (USD)', total_fees);
+            console.log('lowest rate per bitcoin (USD)', lowest_rate);
+            console.log('highest rate per bitcoin (USD)', highest_rate);
+
+            // Withdrawal
+            var total_withdrawal = transactions.total_withdrawal();
+
+            console.log('total withdrawal (BTC)', total_withdrawal);
+
+            // _.range(bpi_chart_data.labels.length);
+
+            buildChart(transactions_chart_data);
         }
     });
 }
 
 function receivedText() {
+    // load bpi
+    var bpi = getJSON('../bpi.json').then(function(result) {
+        console.log("Loaded BPI: ", result);
 
-    var bpi = getJSON('../bpi.json').then(function(result){
-        console.log("BPI: ", result);
-        
         parseBPI(result);
         
-        
-        
+        // parse csv
+        parseCSV(fr.result);
     });
-    
-//    parseCSV(fr.result);
-    
-    //console.log(example);
 }
---
+++ src/js/bitcoin.timeline.js
@@ -0,0 +1,96 @@
+var Transaction = Backbone.Model.extend({
+    isBuy: function() {
+        return this.get('subType') == 'Buy';
+    },
+    
+    isWithdrawal: function() {
+        return this.get('type') == 'Withdrawal';
+    }
+});
+
+var Transactions = Backbone.Collection.extend({
+    model: Transaction,
+
+    initialize: function(options)
+    {
+    },
+
+    withdrawalOnly: function()
+    {
+        return _.reject(this.models, function(obj)
+        {
+            return !obj.isWithdrawal();
+        });
+    },
+
+    buyOnly: function()
+    {
+        return _.reject(this.models, function(obj)
+        {
+            return !obj.isBuy();
+        });
+    },
+ 
+    records_by_day: function(filter)
+    {
+        return _.groupBy(this[filter](), function(item)
+        {
+            return item.get('datestring');
+        });
+    },
+
+    total_by: function(by, filter)
+    {
+        if (filter == 'buy')
+        {
+            filter = 'buyOnly';
+        }
+        else if (filter == 'withdrawal')
+        {
+            filter = 'withdrawalOnly';
+        }
+        return _.reduce(this.records_by_day(filter), function(total, obj)
+        {
+            return total + _.reduce(obj, function(memo, num)
+            {
+                return memo + num.get(by);
+            }, 0);
+        }, 0);
+    },
+
+    total_spent: function()
+    {
+        return this.total_by('value', 'buy');
+    },
+
+    total_bought: function()
+    {
+        return this.total_by('amount', 'buy');
+    },
+
+    total_fees: function()
+    {
+        return this.total_by('fee', 'buy');
+    },
+
+    get_rate: function(item)
+    {
+        return item.get('rate');
+    },
+
+    highest_rate: function()
+    {
+        return _.max(this.buyOnly(), this.get_rate).get('rate');
+    },
+
+    lowest_rate: function()
+    {
+        return _.min(this.buyOnly(), this.get_rate).get('rate');
+    },
+
+    // withdrawal
+    total_withdrawal: function()
+    {
+        return this.total_by('amount', 'withdrawal');
+    }
+});
